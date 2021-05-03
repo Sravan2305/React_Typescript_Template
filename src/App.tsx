6dd79ea5cf949547bbbaf4142a93c2d1b7d7f8b3
Venkata Sravan Kumar Gollapudi <gollapudivenkatasravankumar@gmail.com>
2021-05-03 22:12:43 +0530

    Webpack rules for CSS, Images, SVG's

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-
+import './App.css'
+import Nature from './nature.jpg'
+import Logo from './logo.svg'
 
 function App() {
     return (
-        <div>
-            Hello World!!!
+        <div style={{textAlign:'center', color:'green'}}>
+            <h1> Hello World!!!</h1>
+            <div className='header'>
+                    <img src={Nature} alt='nature' height='200px'/>
+                    <img src={Logo} alt='nature' height='200px'/>
+            </div>
         </div>
     )
 }
